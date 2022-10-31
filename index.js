ca3be2c4aa4a2f028b25834ac0804fe48f0fc374
Dairojhon <112525272+Dairojhon@users.noreply.github.com>
2022-10-31 13:06:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let nombre = "Jhon";
+let apellido = "Rodriguez";
+
+const persona = {
+    nombre,
+    apellido
+}
+
+//localStorage.setItem("persona", JSON.stringify(persona))
+//sessionStorage.setItem("persona", JSON.stringify(persona))
+//document.cookie = "persona="+persona+";max-age="+2*60;
